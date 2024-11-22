b2e38953e796dc0d7fa56ac5f89cc2daa40ef86c
Marlon Fabichacki Pereira <marlon.fa.pereira@hotmail.com>
2024-11-22 13:45:37 -0300

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/* 
+- ler arquivo
+    - cria lista todos as entradas inicio
+    - adiciona final
+
+caso 1
+- ordenar
+- guloso lendo a lista incrementalmente
+
+caso 2
+- guloso procurando pelo menor inicio
+
+ */
+
+struct Aula {
+    int inicio;
+    int final;
+};
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
